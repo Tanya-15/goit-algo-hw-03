f4cc8ee3f0ad6ded214168e3e4b7d23b0725a05f
Tanya-15 <tanya15titova@gmail.com>
2024-11-21 22:29:31 +0200

    second task

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    from_date = datetime.strptime(date, "%Y-%m-%d")
-    now = datetime.now()
-    result = now.toordinal() - from_date.toordinal()
-    return result
+    try:
+        from_date = datetime.strptime(date, "%Y-%m-%d")
+        now = datetime.now()
+        result = now.toordinal() - from_date.toordinal()
+        return result
+    except ValueError:
+        return "Неправильний формат дати. Будь ласка, використовуйте формат РРРР-ММ-ДД."
 
-date = input("Введіть дату в форматі РРРР-ММ-ДД")
-get_days_from_today(date)
+date = input("Введіть дату в форматі РРРР-ММ-ДД: ")
+result = get_days_from_today(date)
 
-print(get_days_from_today(date))
+print(result)
