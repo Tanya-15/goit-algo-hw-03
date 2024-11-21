4d0fb235a469060d6abe505c303c197faff29c47
Tanya-15 <tanya15titova@gmail.com>
2024-11-21 22:35:20 +0200

    first task

--- second.py
+++ second.py
@@ -1,13 +1,19 @@
 import random
+
+
 def get_numbers_ticket(min, max, quantity):
-    if min >= max:
+    # Перевірка на некоректні вхідні дані
+    if min >= max or quantity > (max - min + 1) or min < 0 or quantity <= 0:
         return []
+
     numbers = []
-    while len(numbers)<quantity:
+    while len(numbers) < quantity:
         random_number = random.randint(min, max)
         if random_number not in numbers:
             numbers.append(random_number)
     return numbers
 
+
+# Тестування функції
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
